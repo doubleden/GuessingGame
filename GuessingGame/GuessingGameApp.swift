4c75389fbd3394464a5f1ccad6431dac488f50a6
Denis Denisov <denis.denisov8701@gmail.com>
2024-04-27 21:07:17 +0200

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Denis Denisov on 27/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
